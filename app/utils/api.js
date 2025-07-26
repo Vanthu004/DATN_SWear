8be9f5fe0ba71a41532926b3f726b1a716e52fea
Vanthu204 <lythu2k4lc@gmail.com>
2025-07-26 22:20:07 +0700

    app

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,7 +2,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-const API_BASE_URL = "http://192.168.37.5:3000/api"; //
+const API_BASE_URL = "http://192.168.1.7:3000/api"; //
 
 
 const api = axios.create({
