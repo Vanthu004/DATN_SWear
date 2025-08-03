01f7b279c1b3366f2fa12fa27294184076aea3d0
longvdph46585 <vlong285@gmail.com>
2025-08-03 11:25:57 +0700

    ✨ Update reviewController and review routes to support product_id

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-const API_BASE_URL = "http://192.168.1.85:3000/api"; //
+const API_BASE_URL = "http://172.31.98.252:3000/api"; //
 
 
 const api = axios.create({
